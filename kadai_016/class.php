048b6fc9ffedc53bd729688c75e5424506075145
work2024jun <2024jobac@gmail.com>
2025-02-07 22:04:10 +0900

    Add files via upload

---
+++ kadai_016/class.php
@@ -0,0 +1,45 @@
+<?php
+
+class food {
+public $name;
+public $price;
+
+public function __construct(string $name, int $price) {
+  $this->name = $name;
+  $this->price = $price;
+ }
+
+public function show_price() {
+ echo $this->price;
+}
+
+
+}
+
+class animal {
+
+private $name;
+private $height;
+private $weight;
+
+public function __construct( string $name, int $height, int $weight) {
+  $this->name = $name;
+  $this->height = $height;
+  $this->weight = $weight; 
+  }
+
+public function show_height() {
+echo $this->height;
+}
+
+}
+
+$tomat = new food('とまと', 100) ;
+$cat = new animal('猫', 60, 20);
+print_r($tomat);
+print_r($cat);
+$tomat->show_price();
+echo "<br>";
+$cat->show_height();
+
+?>
